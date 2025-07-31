50c2746e6199e91f38be555fa4d1b878a48b833e
Adwiteeya R Paul <153979848+adwiteeya-r-paul@users.noreply.github.com>
2025-07-31 21:27:01 +0600

    simplified

--- map.py
+++ map.py
@@ -20,7 +20,7 @@ def plot():
 
 import os
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
