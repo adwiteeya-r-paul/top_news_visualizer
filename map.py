b3a945d9bf517741d47f33eb43e2752bb068d05d
Adwiteeya R Paul <153979848+adwiteeya-r-paul@users.noreply.github.com>
2025-08-01 00:53:37 +0600

    api format

--- map.py
+++ map.py
@@ -1,7 +1,8 @@
 # map.py
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, Response
 from youtube import youtube
 from geocode import get_geocode
+import os
 
 app = Flask(__name__)
 
@@ -16,23 +17,25 @@ def serve_index():
 def serve_static_file(path):
     return send_from_directory('static', path)
 
+@app.route("/api-key.js")
+def serve_api_key():
+    api_key = os.environ.get("API_KEY")
+    response_js = f"var API_KEY = '{api_key}';"
+    return Response(response_js, mimetype='application/javascript')
+
 @app.route("/api/plot")
 def plot():
     df = youtube()
     for index, row in df.iterrows():
         country_code = row['Country']
         if country_code not in geocode_cache:
-            # Geocode the country and store the result in the cache
             geocode_cache[country_code] = get_geocode(country_code)
         
-        # Use the cached values
         lat, lon = geocode_cache[country_code]
         df.loc[index, 'Latitude'] = lat
         df.loc[index, 'Longitude'] = lon
 
     return df.to_json(orient='records')
-
-import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
