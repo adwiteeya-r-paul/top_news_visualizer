196b3d438169502bc19d7934763b160bcb894cea
Adwiteeya R Paul <153979848+adwiteeya-r-paul@users.noreply.github.com>
2025-07-31 21:55:14 +0600

    idk

--- geocode.py
+++ geocode.py
@@ -4,7 +4,7 @@ import pycountry
 from geopy.geocoders import GoogleV3
 
 
-geolocator = GoogleV3(api_key="=AIzaSyBaoXhplpt8-FESvK6YxKJi1fUWlnJYYI8")
+geolocator = GoogleV3(api_key="AIzaSyBaoXhplpt8-FESvK6YxKJi1fUWlnJYYI8")
 
 def get_geocode(isocode):
     country = pycountry.countries.get(alpha_2=isocode)
--- map.py
+++ map.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from youtube import youtube
 from geocode import get_geocode
 
